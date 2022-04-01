37d9817178581fd32fe8432c83637e47f0794a81
jeffersondrs <jeffersonduarte@id.uff.br>
2022-03-31 23:21:27 -0300

    final project

---
+++ js/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const painels = document.querySelectorAll('.painel');
+painels.forEach((painel) => {
+    painel.addEventListener('click', () => {
+        removeActiveClasses()
+        painel.classList.add('active')
+    })
+})
+
+function removeActiveClasses(){
+    painels.forEach(painel => {
+        painel.classList.remove('active')
+    })
+}
